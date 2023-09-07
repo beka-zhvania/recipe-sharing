866d74170ef29a9200fe1a616571e5968ae8eb36
beka-zhvania <beka.zhvania@rwth-aachen.de>
2023-09-07 09:50:08 +0200

    add base app with registration and authentication functionalities

---
+++ recipe_sharing/base/forms.py
@@ -0,0 +1,53 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+form_styling = 'w-full py-4 px-6 rounded-xl'
+
+class CustomRegistrationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+    username = forms.CharField(
+        max_length="50",
+        required=True,
+        help_text='Please enter a valid username',
+        widget=forms.TextInput(attrs={
+        'placeholder': "Your username",
+        'class': form_styling
+        })
+    )
+
+    email = forms.EmailField(
+        max_length=100, 
+        required=True, 
+        help_text='Please enter a valid email address.', 
+        widget=forms.EmailInput(attrs={
+        'placeholder': 'Your email address...',
+        'class': form_styling
+        })
+    )
+
+    password1 = forms.CharField(
+        max_length="50",
+        required=True,
+        help_text='Please enter a password',
+        widget=forms.PasswordInput(attrs={
+        'placeholder': "Your password",
+        'class': form_styling
+    })
+    )
+
+    password2 = forms.CharField(
+        max_length="50",
+        required=True,
+        help_text='Please repeat the password',
+        widget=forms.PasswordInput(attrs={
+        'placeholder': "Your password",
+        'class': form_styling
+        })
+    )
--- recipe_sharing/recipe_sharing/settings.py
+++ recipe_sharing/recipe_sharing/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base',
 ]
 
 MIDDLEWARE = [
@@ -79,6 +80,11 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
 
 
 # Password validation
